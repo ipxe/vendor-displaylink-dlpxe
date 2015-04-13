b366e6ec8cb88a0bb93c9503ed27e2b5e2de17dd
Michael Brown <mcb30@ipxe.org>
2015-04-13 16:03:42 +0100

    Change "Error:%08x" to "Code:%08x"
    The PRODUCT_ERROR_URI string is displayed for some non-error
    situations (e.g. the default "unknown" link state).  Avoid confusing
    the user with the potentially misleading text "Error".

--- src/config/local/branding.h
+++ src/config/local/branding.h
@@ -7,7 +7,7 @@
 #undef PRODUCT_TAG_LINE
 #define PRODUCT_TAG_LINE "DisplayLink Network Boot Firmware"
 #undef PRODUCT_ERROR_URI
-#define PRODUCT_ERROR_URI "Error:%08x"
+#define PRODUCT_ERROR_URI "Code:%08x"
 #undef PRODUCT_COMMAND_URI
 #define PRODUCT_COMMAND_URI "Command:%s"
 #undef PRODUCT_SETTING_URI
